23156608a6efa2d24d6e20bc548a6d646468c9eb
Thor <87436831+sanzelda@users.noreply.github.com>
2021-07-19 16:14:43 +0200

    Upload of the searcher

---
+++ PrimNumber.py
@@ -0,0 +1,34 @@
+"""
+This is a program to find prime numbers with python, written by Thor Naughton
+Project started: 04/06/2021
+"""
+
+def brutePrimes():
+    startAt = int(input('Start search at (default is 0) :') or '0')
+    upTo = int(input("End search at : "))
+
+    
+
+    #opens a file Primes.txt to write the prime numbers to
+    with open('Primes.txt', 'w') as numFile:
+
+        #loops through all the numbers
+        for num in range(startAt ,upTo+1):
+            prime = True
+
+            #checks if the number is prime.
+            for i in range(2,num):
+                if (num%i==0):
+                    prime = False
+
+            #Adds number to Primes.txt and prints the prime number to the console.
+            if prime:
+                numFile.write(str(num)+'\n')
+                print(num)
+                    
+
+
+
+brutePrimes()
+
+input('Press Enter to exit...')
